9134814a809aa452362740f0cbd51cb768818973
kseroten <klein.damian46@gmail.com>
2024-03-20 03:01:38 +0100

    Lab2_V2

--- Lab2/Zadanie5.py
+++ Lab2/Zadanie5.py
@@ -1,1 +1,10 @@
+import requests
 from bs4 import BeautifulSoup
+
+response = requests.get("https://www.wp.pl")
+print(response)
+soup = BeautifulSoup(response.content, 'html.parser')
+print(soup)
+links = soup.find_all("a") # Find all elements with the tag <a>
+for link in links:
+  print("Link:", link.get("href"), "Text:", link.string)
