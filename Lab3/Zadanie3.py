15f21e0bbe65fc2bb1caeb91445d26a4bc095aa8
Kseroten1 <klein.damian46@gmail.com>
2024-04-17 09:27:04 +0200

    Lab3_zadanie3

--- Lab3/Zadanie3.py
+++ Lab3/Zadanie3.py
@@ -22,3 +22,25 @@ from student
 join grade on student.student_id = grade.student_id
 where grade.grade >= 4 
 """)
+studentsGrade4 = cursor.fetchall()
+for student in studentsGrade4:
+    print(student)
+
+print("\n Wykladowcy: ")
+cursor.execute("SELECT name, surname, lecture_id from lecturer ")
+lecturers = cursor.fetchall()
+for lecturer in lecturers:
+    print(lecturer)
+
+print("\n wydzial")
+cursor.execute("SELECT faculty.name, student_group.name FROM faculty JOIN student_group ON faculty.faculty_id = student_group.faculty_id")
+dane = cursor.fetchall()
+for wydzial in dane:
+    print(wydzial)
+
+print("\nStudenci z srednia ocen")
+
+cursor.execute(" SELECT Student.student_id, Student.name, Student.surname, AVG(grade.grade) AS avarege FROM Student JOIN grade ON Student.student_id = grade.student_id GROUP BY Student.student_id")
+dane = cursor.fetchall()
+for student in dane:
+    print(student)
